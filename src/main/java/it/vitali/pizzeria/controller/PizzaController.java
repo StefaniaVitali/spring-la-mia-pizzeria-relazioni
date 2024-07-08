bd782028be8de204612744cd7f8f9ce30162115e
StefaniaVitali <stefy.egypt@hotmail.it>
2024-07-08 10:53:21 +0200

    add: search by Id

--- src/main/java/it/vitali/pizzeria/controller/PizzaController.java
+++ src/main/java/it/vitali/pizzeria/controller/PizzaController.java
@@ -1,12 +1,15 @@
 package it.vitali.pizzeria.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import it.vitali.pizzeria.model.Pizza;
 import it.vitali.pizzeria.repository.PizzaRepository;
@@ -19,15 +22,29 @@ public class PizzaController {
 	private PizzaRepository repository;
 	
 	@GetMapping
-	public String index(Model model) {
+	public String index(Model model, @RequestParam(name="nome", required = false) String nome) {
 		
-		List <Pizza> pizze = repository.findAll();
+		List <Pizza> pizze = new ArrayList<>();
 		
-		model.addAttribute("list", pizze);		
 		
+		if(nome == null || nome.isBlank()) {
+			pizze = repository.findAll();
+		} else {
+			pizze = repository.findByNome(nome);
+		}
+		
+		model.addAttribute("list", pizze);	
 		return "/pizzeria/index";
 		
+	}	
+	
+	@GetMapping("/show/{id}")
+	public String show(@PathVariable("id") Integer pizzaId, Model model) {
+		
+		model.addAttribute("pizza", repository.getReferenceById(pizzaId));
+		return "pizzeria/show";
 	}
+	
 
 //fine classe
 }
--- src/main/java/it/vitali/pizzeria/repository/PizzaRepository.java
+++ src/main/java/it/vitali/pizzeria/repository/PizzaRepository.java
@@ -1,9 +1,13 @@
 package it.vitali.pizzeria.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import it.vitali.pizzeria.model.Pizza;
 
 public interface PizzaRepository extends JpaRepository<Pizza,Integer> {
+	
+	List<Pizza> findByNome(String nome);
 
 }
