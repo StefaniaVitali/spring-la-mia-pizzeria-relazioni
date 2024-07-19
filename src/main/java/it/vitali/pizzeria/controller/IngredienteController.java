f5d6345cfcd6f38633e9aa72b0c290907dd36f95
StefaniaVitali <stefy.egypt@hotmail.it>
2024-07-19 10:12:20 +0200

    ADD: anchor di navigazione

--- src/main/java/it/vitali/pizzeria/controller/IngredienteController.java
+++ src/main/java/it/vitali/pizzeria/controller/IngredienteController.java
@@ -6,6 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -35,7 +36,16 @@ public class IngredienteController {
 			return "ingredienti/index";
 		}		
 		ingredienteRepository.save(ingrediente);
-		return "ingredienti/index";
+		return "redirect:/ingrediente";
+	}
+	
+	@PostMapping("/delete/{id}")
+	public String delete (@PathVariable("id") Integer id) {
+		
+		ingredienteRepository.deleteById(id);
+		
+		return "redirect:/ingrediente";
+		
 	}
 	
 	
